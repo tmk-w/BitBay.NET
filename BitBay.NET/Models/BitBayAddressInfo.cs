e7e23d81d0a347aca412f4bb0350644251c54c9b
Tomasz Wnuk <tomasz.wnuk9@gmail.com>
2019-02-15 15:58:34 +0100

    Add all missing currencies to getinfo endpoint

--- BitBay.NET/Models/BitBayAddressInfo.cs
+++ BitBay.NET/Models/BitBayAddressInfo.cs
@@ -16,5 +16,27 @@ namespace BitBay.NET.Models
         public string ETH { get; set; }
         public string LTC { get; set; }
         public string DASH { get; set; }
+        public string FTO { get; set; }
+        public string EXY { get; set; }
+        public string PAY { get; set; }
+        public string GNT { get; set; }
+        public string BOB { get; set; }
+        public string XLM { get; set; }
+        public string TRX { get; set; }
+        public string ZRX { get; set; }
+        public string BSV { get; set; }
+        public string OMG { get; set; }
+        public string LML { get; set; }
+        public string AMLT { get; set; }
+        public string BCP { get; set; }
+        public string XBX { get; set; }
+        public string KZC { get; set; }
+        public string BAT { get; set; }
+        public string ZEC { get; set; }
+        public string USDC { get; set; }
+        public string REP { get; set; }
+        public string XIN { get; set; }
+        public string XMR { get; set; }
+        public string NEU { get; set; }
     }
 }
--- BitBay.NET/Models/BitBayBalancesInfo.cs
+++ BitBay.NET/Models/BitBayBalancesInfo.cs
@@ -16,5 +16,27 @@ namespace BitBay.NET.Models
         public BitBayCurrencyInfo ETH { get; set; }
         public BitBayCurrencyInfo LTC { get; set; }
         public BitBayCurrencyInfo DASH { get; set; }
+        public BitBayCurrencyInfo FTO { get; set; }
+        public BitBayCurrencyInfo EXY { get; set; }
+        public BitBayCurrencyInfo PAY { get; set; }
+        public BitBayCurrencyInfo GNT { get; set; }
+        public BitBayCurrencyInfo BOB { get; set; }
+        public BitBayCurrencyInfo XLM { get; set; }
+        public BitBayCurrencyInfo TRX { get; set; }
+        public BitBayCurrencyInfo ZRX { get; set; }
+        public BitBayCurrencyInfo BSV { get; set; }
+        public BitBayCurrencyInfo OMG { get; set; }
+        public BitBayCurrencyInfo LML { get; set; }
+        public BitBayCurrencyInfo AMLT { get; set; }
+        public BitBayCurrencyInfo BCP { get; set; }
+        public BitBayCurrencyInfo XBX { get; set; }
+        public BitBayCurrencyInfo KZC { get; set; }
+        public BitBayCurrencyInfo BAT { get; set; }
+        public BitBayCurrencyInfo ZEC { get; set; }
+        public BitBayCurrencyInfo USDC { get; set; }
+        public BitBayCurrencyInfo REP { get; set; }
+        public BitBayCurrencyInfo XIN { get; set; }
+        public BitBayCurrencyInfo XMR { get; set; }
+        public BitBayCurrencyInfo NEU { get; set; }
     }
 }

7ad45b8fda5574d7581d780950ce8cbc561b7280
Tomasz Wnuk <tomasz.wnuk9@gmail.com>
2019-02-18 11:39:18 +0100

    Add project with unit tests

---
+++ BitBay.NET.UnitTests/BitBayClientTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitBay.NET.UnitTests
+{
+
+    [TestClass]
+    public class BitBayClientTests
+    {
+        private BitBayClient client;
+
+        [TestInitialize]
+        public void Init()
+        {
+            client = new BitBayClient(new ClientConfiguration());
+        }
+
+        [TestMethod]
+        public void GetInfoAsync_ReturnsProperData()
+        {
+            
+        }
+    }
+}
